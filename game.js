fd11379c6c5a5d70e604e73255201e5b6ca1a86a
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-04 21:02:08 +0200

    start setting up images for different facing directions

--- game.js
+++ game.js
@@ -2,18 +2,38 @@ class Game {
     constructor() {
         this.player;
         this.backgroundImage;
+        this.playerDown;
+        this.playerUp;
+        this.playerRight;
+        this.playerLeft;
         this.playerImage;
+        this.playerImages;
     }
     setup() {
+        this.playerImages = {
+            up : 'this.playerUp',
+            down: 'this.playerUp',
+            left : 'this.playerLeft',
+            right : 'this.playerRight'
+        };
         this.player = new Player();
         this.player.setup(0,0);
+        this.playerImage = 'down';
+        console.log('current player image '+ this.playerImage);
     }
     preLoad() {
         this.backgroundImage = loadImage('assets/individual-images/floor.png');
-        this.playerImage = loadImage('assets/individual-images/player/playerDown1.png');
+        this.playerDown = loadImage('assets/individual-images/player/playerDown1.png');
+        this.playerUp = loadImage('assets/individual-images/player/playerUp1.png');
+        this.playerRight = loadImage('assets/individual-images/player/playerRight1.png');
+        this.playerLeft = loadImage('assets/individual-images/player/playerLeft1.png');
+
     }
     draw() {
         image(this.backgroundImage, 0, 0, width, height)
         this.player.draw();
+    }
+    changePlayerImage(face){
+        this.playerImage = this.playerImages[this.player.facing];
     }
 }
--- player.js
+++ player.js
@@ -3,6 +3,7 @@ class Player {
         this.xPos;
         this.yPos;
         this.facing;
+        this.parsedXPos;
     }
     setup(initialXPos, initialYPos) {
         this.xPos = initialXPos || 0;
@@ -11,26 +12,64 @@ class Player {
     }
 
     draw() {
-        image(game.playerImage, this.xPos, this.yPos, 50, 50)
-        //console.log('player draw called')
+
+        //THIS IS WHERE THE PROBLEM IS!
+        //image(game.playerImage, this.xPos * 50, this.yPos * 50, 50, 50);
+        //now wanted...
+        image(game.playerDown, this.xPos * 50, this.yPos * 50, 50, 50);
+
     }
 
     moveDown(inputDirection) {
+        if (this.facing === inputDirection) {
+            if (this.yPos < 9) {
+                this.yPos += 1;
+            }
+        } else {
+            this.facing = inputDirection;
+            game.changePlayerImage(inputDirection);
+        }
         this.facing = inputDirection;
         console.log('player facing: ' + this.facing + '[should be down]')
     }
 
     moveUp(inputDirection) {
+        if (this.facing === inputDirection) {
+            if (this.yPos >= 1) {
+                this.yPos -= 1;
+            }
+        } else {
+            this.facing = inputDirection;
+            game.changePlayerImage(inputDirection);
+        }
+
         this.facing = inputDirection;
         console.log('player facing: ' + this.facing + '[should be up]')
     }
 
     moveRight(inputDirection) {
+        if (this.facing === inputDirection) {
+            if (this.xPos < 9) {
+                this.xPos += 1;
+            }
+        } else {
+            this.facing = inputDirection;
+            game.changePlayerImage(inputDirection);
+        }
+
         this.facing = inputDirection;
         console.log('player facing: ' + this.facing + '[should be right]')
     }
 
     moveLeft(inputDirection) {
+        if (this.facing === inputDirection) {
+            if (this.xPos >= 1) {
+                this.xPos -= 1;
+            }
+        } else {
+            this.facing = inputDirection;
+            game.changePlayerImage(inputDirection);
+        }
         this.facing = inputDirection;
         console.log('player facing: ' + this.facing + '[should be left]')
     }
