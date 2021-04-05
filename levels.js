740ae6b759287836899191422c97ed5663dc31a6
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-05 13:58:41 +0200

    add a layout for first level

--- levels.js
+++ levels.js
@@ -5,16 +5,16 @@ class Levels {
         //this is just to get it working, eventually make an object containing all levels and then
         //refer to them by key
         this.levelOne = [
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'b', 'b', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+            ['o', 'o', 'b', 'b', 'b', 'b', 'b', 'b', 'o', 'o'],
+            ['o', 'o', 'b', 'b', 'b', 'b', 'b', 'b', 'o', 'd'],
+            ['o', 'o', 'b', 'b', 'b', 'b', 'b', 'b', 'o', 'o'],
+            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'b'],
+            ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'b'],
+            ['b', 'b', 'b', 'o', 'b', 'b', 'o', 'o', 'o', 'b'],
+            ['b', 'b', 'b', 'o', 'b', 'b', 'o', 'b', 'b', 'b'],
+            ['b', 'b', 'b', 'o', 'b', 'b', 'o', 'b', 'b', 'b'],
+            ['b', 'b', 'b', 'o', 'b', 'b', 'o', 'b', 'b', 'b'],
+            ['b', 'b', 'b', 'gr', 'b', 'b', 'gr', 'b', 'b', 'b'],
           ];
     }
 }
