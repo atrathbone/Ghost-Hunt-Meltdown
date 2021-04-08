e312c51931a596838ccd55dc4e25d2bab293ccb3
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-08 17:45:31 +0200

    edit level which was too hard

--- animation.js
+++ animation.js
@@ -3,11 +3,9 @@ class Animation {
         this.frameCounter = 0;
         this.width = width || 50;
         this.height = height || 50;
-        // this.amountOfFrames = 5;  
     }
     
     animate(loadedAnimationImages, x, y, frames) {
-        // if(!frames===NaN){this.amountOfFrames = frames;}
         image(loadedAnimationImages[this.frameCounter], x, y, this.width, this.height);
         if (this.frameCounter < frames) {
             this.frameCounter += 1;
